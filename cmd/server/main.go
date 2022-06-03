103004997ddd2f0c5259b9ec2ddee6c14176fcd8
Guilherme Tadeu <gtadeu0147@gmail.com>
2022-06-03 14:59:28 -0300

    Creating database package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,9 @@
 package main
 
+import db "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+
 func main() {
 
 	// sellers, warehouses, sections, products, employees, buyers
-	var _, _, _, _, _, _ = CreateDatabases()
+	var _, _, _, _, _, _ = db.CreateDatabases()
 }
