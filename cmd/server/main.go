dc8839f83c0efecb0ae176db53dd3eea473bbba4
anagiorgiani <cavalheiro.anacarolina@gmail.com>
2022-06-05 14:22:28 -0300

    requirement 03

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,32 @@
 package main
 
-import db "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+import (
+	controller "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/controllers"
+	db "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+	rp "github.com/GuiTadeu/mercado-fresh-panic/internal/sections"
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
 
 	// sellers, warehouses, sections, products, employees, buyers
-	var _, _, _, _, _, _ = db.CreateDatabases()
+	var _, _, sections, _, _, _ = db.CreateDatabases()
+
+	rSections := rp.NewRepository(sections)
+
+	sSections := rp.NewService(rSections)
+
+	cSections := controller.NewController(sSections)
+
+	r := gin.Default()
+
+	sectionRoutes := r.Group("/sections")
+
+	sectionRoutes.GET("/", cSections.GetAll())
+	sectionRoutes.GET("/id", cSections.Get())
+	sectionRoutes.POST("/", cSections.Create())
+	sectionRoutes.PUT("/id", cSections.Update())
+	sectionRoutes.DELETE("/id", cSections.Delete())
+
+	r.Run()
 }
