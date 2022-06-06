0fb762d1cede27bb989575ee2f61a01be1d9ca92
Guilherme Tadeu <gtadeu0147@gmail.com>
2022-06-06 19:41:53 -0300

    Refactoring PATCH Method

--- cmd/server/controllers/sections.go
+++ cmd/server/controllers/sections.go
@@ -8,8 +8,7 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-type request struct {
-	Id                 uint64  `json:"id"`
+type CreateSectionRequest struct {
 	Number             uint64  `json:"number" binding:"required"`
 	CurrentTemperature float32 `json:"current_temperature" binding:"required"`
 	MinimumTemperature float32 `json:"minimum_temperature" binding:"required"`
@@ -18,6 +17,15 @@ type request struct {
 	MaximumCapacity    uint32  `json:"maximum_capacity" binding:"required"`
 	WarehouseId        uint64  `json:"warehouse_id" binding:"required"`
 	ProductTypeId      uint64  `json:"product_type_id" binding:"required"`
+}
+
+type UpdateSectionRequest struct {
+	Number             uint64  `json:"number"`
+	CurrentTemperature float32 `json:"current_temperature"`
+	MinimumTemperature float32 `json:"minimum_temperature"`
+	CurrentCapacity    uint32  `json:"current_capacity"`
+	MinimumCapacity    uint32  `json:"minimum_capacity"`
+	MaximumCapacity    uint32  `json:"maximum_capacity"`
 }
 
 type sectionController struct {
@@ -72,8 +80,8 @@ func (c *sectionController) Get() gin.HandlerFunc {
 
 func (c *sectionController) Create() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		var req request
 
+		var req CreateSectionRequest
 		err := ctx.ShouldBindJSON(&req)
 
 		if err != nil {
@@ -109,51 +117,37 @@ func (c *sectionController) Create() gin.HandlerFunc {
 
 		ctx.JSON(200, web.NewResponse(200, addedSection, ""))
 	}
-
 }
 
 func (c *sectionController) Update() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
-		id, err := strconv.Atoi(ctx.Param("id"))
 
+		var request UpdateSectionRequest
+		err := ctx.ShouldBindJSON(&request)
+
+		id, err := strconv.ParseUint(ctx.Param("id"), 10, 64)
 		if err != nil {
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(400, web.NewResponse(400, nil, "Section id binding error"))
 			return
 		}
 
-		var req request
-
-		err = ctx.ShouldBindJSON(&req)
-
-		if err != nil {
-			ctx.JSON(422, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		_, err = c.sectionService.Get(uint64(id))
-		if err != nil {
-			ctx.JSON(404, gin.H{
-				"error": err.Error(),
-			})
-			return
-		}
-
-		updatedSection, err := c.sectionService.Update(uint64(id), req.Number, req.CurrentTemperature, req.MinimumTemperature, req.CurrentCapacity, req.MinimumCapacity, req.MaximumCapacity, req.WarehouseId, req.ProductTypeId)
+		updatedSection, err := c.sectionService.Update(
+			id,
+			request.Number,
+			request.CurrentTemperature,
+			request.MinimumTemperature,
+			request.CurrentCapacity,
+			request.MinimumCapacity,
+			request.MaximumCapacity,
+		)
 
 		if err != nil {
-			ctx.JSON(500, gin.H{
-				"error": err.Error(),
-			})
+			ctx.JSON(500, web.NewResponse(500, nil, err.Error()))
 			return
 		}
 
 		ctx.JSON(200, web.NewResponse(200, updatedSection, ""))
 	}
-
 }
 
 func (c *sectionController) Delete() gin.HandlerFunc {
--- internal/sections/service.go
+++ internal/sections/service.go
@@ -1,12 +1,17 @@
 package sections
 
-import db "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+import (
+	"errors"
+
+	db "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+	"github.com/imdario/mergo"
+)
 
 type SectionService interface {
 	GetAll() ([]db.Section, error)
 	Get(id uint64) (db.Section, error)
 	Create(id uint64, number uint64, currentTemperature float32, minimumTemperature float32, currentCapacity uint32, minimumCapacity uint32, maximumCapacity uint32, warehouseId uint64, productTypeId uint64) (db.Section, error)
-	Update(id uint64, number uint64, currentTemperature float32, minimumTemperature float32, currentCapacity uint32, minimumCapacity uint32, maximumCapacity uint32, warehouseId uint64, productTypeId uint64) (db.Section, error)
+	Update(id uint64, number uint64, currentTemperature float32, minimumTemperature float32, currentCapacity uint32, minimumCapacity uint32, maximumCapacity uint32) (db.Section, error)
 	Delete(id uint64) error
 	GetNextId() uint64
 }
@@ -33,8 +38,29 @@ func (s *sectionService) Create(id uint64, number uint64, currentTemperature flo
 	return s.sectionRepository.Create(id, number, currentTemperature, minimumTemperature, currentCapacity, minimumCapacity, maximumCapacity, warehouseId, productTypeId)
 }
 
-func (s *sectionService) Update(id uint64, number uint64, currentTemperature float32, minimumTemperature float32, currentCapacity uint32, minimumCapacity uint32, maximumCapacity uint32, warehouseId uint64, productTypeId uint64) (db.Section, error) {
-	return s.sectionRepository.Update(id, number, currentTemperature, minimumTemperature, currentCapacity, minimumCapacity, maximumCapacity, warehouseId, productTypeId)
+func (s *sectionService) Update(
+	id uint64, newNumber uint64, newCurrentTemperature float32,
+	newMinimumTemperature float32, newCurrentCapacity uint32,
+	newMinimumCapacity uint32, newMaximumCapacity uint32,
+) (db.Section, error) {
+
+	foundSection, err := s.Get(id)
+	if err != nil {
+		return db.Section{}, errors.New("Section not found")
+	}
+
+	updatedSection := db.Section{
+		Id:                 id,
+		Number:             newNumber,
+		CurrentTemperature: newCurrentTemperature,
+		MinimumTemperature: newMinimumTemperature,
+		CurrentCapacity:    newCurrentCapacity,
+		MinimumCapacity:    newMinimumCapacity,
+		MaximumCapacity:    newMaximumCapacity,
+	}
+
+	mergo.Merge(&foundSection, updatedSection, mergo.WithOverride)
+	return s.sectionRepository.Update(id, foundSection)
 }
 
 func (s *sectionService) Delete(id uint64) error {
