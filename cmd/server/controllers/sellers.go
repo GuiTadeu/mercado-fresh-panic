e4a8b0d7cfa62839ceb7991febbcdb47ec7d2bb4
tales-lopes-meli <105742741+tales-lopes-meli@users.noreply.github.com>
2022-06-08 15:49:57 -0300

    feature(sellers): Using Response struct on controller

--- cmd/server/controllers/sellers.go
+++ cmd/server/controllers/sellers.go
@@ -5,6 +5,7 @@ import (
 	"strconv"
 
 	"github.com/GuiTadeu/mercado-fresh-panic/internal/sellers"
+	"github.com/GuiTadeu/mercado-fresh-panic/pkg/web"
 	"github.com/gin-gonic/gin"
 )
 
@@ -29,9 +30,7 @@ func (control *SellersController) FindAll() gin.HandlerFunc {
 			return
 		}
 
-		ctx.JSON(statusCode, gin.H{
-			"data": s,
-		})
+		ctx.JSON(statusCode, web.NewResponse(statusCode, s, ""))
 	}
 }
 
@@ -43,9 +42,7 @@ func (control *SellersController) FindOne() gin.HandlerFunc {
 		id, err := strconv.ParseUint(idParam, 0, 64)
 
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": "ID in wrong format",
-			})
+			ctx.JSON(http.StatusInternalServerError, web.NewResponse(http.StatusInternalServerError, nil, "ID in wrong format"))
 			return
 		}
 
@@ -58,9 +55,7 @@ func (control *SellersController) FindOne() gin.HandlerFunc {
 			return
 		}
 
-		ctx.JSON(statusCode, gin.H{
-			"data": s,
-		})
+		ctx.JSON(statusCode, web.NewResponse(statusCode, s, ""))
 	}
 }
 
@@ -70,24 +65,18 @@ func (control *SellersController) Create() gin.HandlerFunc {
 		var req sellerRequest
 
 		if err := ctx.ShouldBindJSON(&req); err != nil {
-			ctx.JSON(http.StatusUnprocessableEntity, gin.H{
-				"message": err.Error(),
-			})
+			ctx.JSON(http.StatusUnprocessableEntity, web.NewResponse(http.StatusUnprocessableEntity, nil, err.Error()))
 			return
 		}
 
 		s, statusCode, err := control.service.Create(req.Cid, req.CompanyName, req.Address, req.Telephone)
 
 		if err != nil {
-			ctx.JSON(statusCode, gin.H{
-				"message": err.Error(),
-			})
+			ctx.JSON(statusCode, web.NewResponse(statusCode, nil, err.Error()))
 			return
 		}
 
-		ctx.JSON(statusCode, gin.H{
-			"data": s,
-		})
+		ctx.JSON(statusCode, web.NewResponse(statusCode, s, ""))
 	}
 }
 
@@ -101,31 +90,23 @@ func (control *SellersController) Update() gin.HandlerFunc {
 		id, err := strconv.ParseUint(idParam, 0, 64)
 
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": "ID in wrong format",
-			})
+			ctx.JSON(http.StatusInternalServerError, web.NewResponse(http.StatusInternalServerError, nil, "ID in wrong format"))
 			return
 		}
 
 		if err := ctx.ShouldBindJSON(&req); err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": err.Error(),
-			})
+			ctx.JSON(http.StatusInternalServerError, web.NewResponse(http.StatusInternalServerError, nil, err.Error()))
 			return
 		}
 
 		s, statusCode, err := control.service.Update(id, req.Cid, req.CompanyName, req.Address, req.Telephone)
 
 		if err != nil {
-			ctx.JSON(statusCode, gin.H{
-				"message": err.Error(),
-			})
+			ctx.JSON(statusCode, web.NewResponse(statusCode, nil, err.Error()))
 			return
 		}
 
-		ctx.JSON(statusCode, gin.H{
-			"data": s,
-		})
+		ctx.JSON(statusCode, web.NewResponse(statusCode, s, ""))
 	}
 }
 
@@ -137,30 +118,24 @@ func (control *SellersController) Delete() gin.HandlerFunc {
 		id, err := strconv.ParseUint(idParam, 0, 64)
 
 		if err != nil {
-			ctx.JSON(http.StatusInternalServerError, gin.H{
-				"message": "ID in wrong format",
-			})
+			ctx.JSON(http.StatusInternalServerError, web.NewResponse(http.StatusInternalServerError, nil, "ID in wrong format"))
 			return
 		}
 
 		statusCode, err := control.service.Delete(id)
 
 		if err != nil {
-			ctx.JSON(statusCode, gin.H{
-				"message": err.Error(),
-			})
+			ctx.JSON(statusCode, web.NewResponse(statusCode, nil, err.Error()))
 			return
 		}
 
-		ctx.JSON(statusCode, gin.H{
-			"message": "success on delete",
-		})
+		ctx.JSON(statusCode, web.NewResponse(statusCode, nil, ""))
 	}
 }
 
 type sellerRequest struct {
-	Cid         uint64 `json:"cid"`
-	CompanyName string `json:"company_name"`
-	Address     string `json:"address"`
-	Telephone   string `json:"telephone"`
+	Cid         uint64 `json:"cid" binding:"required"`
+	CompanyName string `json:"company_name" binding:"required"`
+	Address     string `json:"address" binding:"required"`
+	Telephone   string `json:"telephone" binding:"required"`
 }
