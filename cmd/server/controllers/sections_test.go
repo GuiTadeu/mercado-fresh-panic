24d4f9bfcda63edd79d2178d3321f0bfd607be17
Ana Carolina Cavalheiro Giorgiani <cavalheiro.anacarolina@gmail.com>
2022-06-24 17:10:03 -0300

    feature section routes tests

---
+++ cmd/server/controllers/sections_test.go
@@ -0,0 +1,332 @@
+package controller
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	db "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+	"github.com/GuiTadeu/mercado-fresh-panic/internal/sections"
+	"github.com/GuiTadeu/mercado-fresh-panic/pkg/web"
+
+	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_Create_201(t *testing.T) {
+
+	validSection := db.Section{
+		Number:             4,
+		CurrentTemperature: 1.0,
+		MinimumTemperature: 1.0,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	jsonValue, _ := json.Marshal(validSection)
+	requestBody := bytes.NewBuffer(jsonValue)
+
+	mockService := mockSectionService{
+		result: validSection,
+		err:    nil,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("POST", "/api/v1/sections", requestBody)
+	router.ServeHTTP(response, request)
+
+	responseData := db.Section{}
+	decodeWebResponse(response, &responseData)
+
+	assert.Equal(t, 201, response.Code)
+	assert.Equal(t, validSection, responseData)
+}
+
+func Test_Create_422(t *testing.T) {
+
+	invalidSection := db.Section{}
+	jsonValue, _ := json.Marshal(invalidSection)
+	requestBody := bytes.NewBuffer(jsonValue)
+
+	mockService := mockSectionService{}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("POST", "/api/v1/sections", requestBody)
+	router.ServeHTTP(response, request)
+
+	assert.Equal(t, 422, response.Code)
+}
+
+func Test_Create_409(t *testing.T) {
+
+	validSection := db.Section{
+		Id:                 1,
+		Number:             4,
+		CurrentTemperature: 1.0,
+		MinimumTemperature: 1.0,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	jsonValue, _ := json.Marshal(validSection)
+	requestBody := bytes.NewBuffer(jsonValue)
+
+	mockService := mockSectionService{
+		result: db.Section{},
+		err:    sections.ExistsSectionNumberError,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("POST", "/api/v1/sections", requestBody)
+	router.ServeHTTP(response, request)
+
+	assert.Equal(t, 409, response.Code)
+}
+
+func Test_GetAll_200(t *testing.T) {
+
+	sectionList := []db.Section{
+		{
+			Id:                 1,
+			Number:             4,
+			CurrentTemperature: 1.0,
+			MinimumTemperature: 1.0,
+			CurrentCapacity:    1,
+			MinimumCapacity:    1,
+			MaximumCapacity:    1,
+			WarehouseId:        1,
+			ProductTypeId:      1,
+		},
+		{
+			Id:                 2,
+			Number:             5,
+			CurrentTemperature: 1.5,
+			MinimumTemperature: 1.5,
+			CurrentCapacity:    2,
+			MinimumCapacity:    2,
+			MaximumCapacity:    2,
+			WarehouseId:        2,
+			ProductTypeId:      2,
+		},
+		{
+			Id:                 3,
+			Number:             6,
+			CurrentTemperature: 2.0,
+			MinimumTemperature: 2.0,
+			CurrentCapacity:    3,
+			MinimumCapacity:    3,
+			MaximumCapacity:    3,
+			WarehouseId:        3,
+			ProductTypeId:      3,
+		},
+	}
+
+	mockService := mockSectionService{
+		result: sectionList,
+		err:    nil,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/api/v1/sections", nil)
+	router.ServeHTTP(response, request)
+
+	responseData := []db.Section{}
+	decodeWebResponse(response, &responseData)
+
+	assert.Equal(t, 200, response.Code)
+	assert.Equal(t, sectionList, responseData)
+}
+
+func Test_Get_200(t *testing.T) {
+
+	foundSection := db.Section{
+		Id:                 1,
+		Number:             4,
+		CurrentTemperature: 1.0,
+		MinimumTemperature: 1.0,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	mockService := mockSectionService{
+		result: foundSection,
+		err:    nil,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/api/v1/sections/666", nil)
+	router.ServeHTTP(response, request)
+
+	assert.Equal(t, 200, response.Code)
+}
+
+func Test_Get_404(t *testing.T) {
+
+	mockService := mockSectionService{
+		result: db.Section{},
+		err:    sections.SectionNotFoundError,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("GET", "/api/v1/sections/666", nil)
+	router.ServeHTTP(response, request)
+
+	assert.Equal(t, 404, response.Code)
+}
+
+func Test_Update_200(t *testing.T) {
+
+	sectionToUpdate := db.Section{
+		Id:                 1,
+		Number:             4,
+		CurrentTemperature: 1.0,
+		MinimumTemperature: 1.0,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	jsonValue, _ := json.Marshal(sectionToUpdate)
+	requestBody := bytes.NewBuffer(jsonValue)
+
+	updatedSection := db.Section{
+		Id:                 2,
+		Number:             5,
+		CurrentTemperature: 1.5,
+		MinimumTemperature: 1.5,
+		CurrentCapacity:    2,
+		MinimumCapacity:    2,
+		MaximumCapacity:    2,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	mockService := mockSectionService{
+		result: updatedSection,
+		err:    nil,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("PATCH", "/api/v1/sections/1", requestBody)
+	router.ServeHTTP(response, request)
+
+	responseData := db.Section{}
+	decodeWebResponse(response, &responseData)
+
+	assert.Equal(t, 200, response.Code)
+	assert.Equal(t, updatedSection, responseData)
+}
+
+func Test_Update_404(t *testing.T) {
+
+	sectionToUpdate := db.Section{
+		Id:                 1,
+		Number:             4,
+		CurrentTemperature: 1.0,
+		MinimumTemperature: 1.0,
+		CurrentCapacity:    1,
+		MinimumCapacity:    1,
+		MaximumCapacity:    1,
+		WarehouseId:        1,
+		ProductTypeId:      1,
+	}
+
+	jsonValue, _ := json.Marshal(sectionToUpdate)
+	requestBody := bytes.NewBuffer(jsonValue)
+
+	mockService := mockSectionService{
+		result: db.Section{},
+		err:    sections.SectionNotFoundError,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("PATCH", "/api/v1/sections/1", requestBody)
+	router.ServeHTTP(response, request)
+
+	responseData := db.Section{}
+	decodeWebResponse(response, &responseData)
+
+	assert.Equal(t, 404, response.Code)
+}
+
+func Test_Delete_204(t *testing.T) {
+
+	mockService := mockSectionService{
+		result: db.Section{},
+		err:    nil,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("DELETE", "/api/v1/sections/1", nil)
+	router.ServeHTTP(response, request)
+
+	assert.Equal(t, 204, response.Code)
+}
+
+func Test_Delete_404(t *testing.T) {
+
+	mockService := mockSectionService{
+		result: db.Section{},
+		err:    sections.SectionNotFoundError,
+	}
+
+	router := setupRouter(mockService)
+
+	response := httptest.NewRecorder()
+	request, _ := http.NewRequest("DELETE", "/api/v1/sections/", nil)
+	router.ServeHTTP(response, request)
+
+	assert.Equal(t, 404, response.Code)
+}
+
+func decodeWebResponse(response *httptest.ResponseRecorder, responseData any) {
+	responseStruct := web.Response{}
+	json.Unmarshal(response.Body.Bytes(), &responseStruct)
+
+	jsonData, _ := json.Marshal(responseStruct.Data)
+	json.Unmarshal(jsonData, &responseData)
+}
+
+func setupRouter(mockService mockSectionService) *gin.Engine {
+	controller := NewSectionController(mockService)
+
+	router := gin.Default()
+	router.POST("/api/v1/sections", controller.Create())
+	router.GET("/api/v1/sections", controller.GetAll())
+	router.GET("/api/v1/sections/:id", controller.Get())
+	router.PATCH("/api/v1/sections/:id", controller.Update())
+	router.DELETE("/api/v1/sections/:id", controller.Delete())
+
+	return router
+}
--- go.mod
+++ go.mod
@@ -8,15 +8,23 @@ require (
 )
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/stretchr/objx v0.4.0 // indirect
+	github.com/stretchr/testify v1.7.3 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
+
+require (
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.0 // indirect
 	github.com/goccy/go-json v0.9.7 // indirect
-	github.com/imdario/mergo v0.3.13 
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.1 // indirect
