0f0fab445e85981643b1514fe4b467de58543fb7
tales-lopes-meli <105742741+tales-lopes-meli@users.noreply.github.com>
2022-07-06 12:04:57 -0300

    initial development of refactor_seller

--- internal/sellers/repository.go
+++ internal/sellers/repository.go
@@ -1,16 +1,36 @@
 package sellers
 
 import (
-	"fmt"
+	"database/sql"
+	"log"
 
 	"github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+	_ "github.com/go-sql-driver/mysql"
 )
 
-var initialId uint64 = 0
+const (
+	FindAllQuery = "SELECT id, cid, company_name, address, telephone, locality_id FROM sellers"
+	FindOneQuery = "SELECT id, cid, company_name, address, telephone, locality_id FROM sellers WHERE id = ?"
+	CreateQuery  = "INSERT INTO sellers(cid, company_name, address, telephone, locality_id) VALUES(?, ?, ?, ?, ?)"
+	DeleteQuery  = "DELETE FROM sellers WHERE id = ?"
+	FindCidQuery = "SELECT cid FROM sellers WHERE cid = ?"
+	UpdateQuery  = `
+		UPDATE 
+			sellers 
+		SET
+			cid = ?,
+			company_name = ?,
+			address = ?,
+			telephone = ?,
+			locality_id = ?
+		WHERE 
+			id = ?
+	`
+)
 
 type Repository interface {
 	FindAll() ([]database.Seller, error)
-	Create(cid uint64, companyName string, address string, telephone string) (database.Seller, error)
+	Create(cid uint64, companyName string, address string, telephone string, localityId string) (database.Seller, error)
 	FindOne(id uint64) (database.Seller, error)
 	Update(seller database.Seller) (database.Seller, error)
 	Delete(id uint64) error
@@ -22,67 +42,128 @@ type repository struct {
 }
 
 func (r *repository) FindAll() ([]database.Seller, error) {
-	return r.db, nil
+	var sellers []database.Seller
+
+	db := database.StorageDB
+
+	rows, err := db.Query(FindAllQuery)
+
+	if err != nil {
+		log.Fatal(err)
+		return []database.Seller{}, err
+	}
+
+	for rows.Next() {
+		var seller database.Seller
+		if err := rows.Scan(&seller.Id, &seller.Cid, &seller.CompanyName, &seller.Address, &seller.Telephone, &seller.LocalityId); err != nil {
+			log.Println(err.Error())
+			return []database.Seller{}, err
+		}
+
+		sellers = append(sellers, seller)
+	}
+
+	return sellers, nil
 }
 
 func (r *repository) FindOne(id uint64) (database.Seller, error) {
-	for _, seller := range r.db {
-		if seller.Id == id {
-			return seller, nil
-		}
+	var seller database.Seller
+
+	db := database.StorageDB
+
+	err := db.QueryRow(FindOneQuery, id).Scan(&seller.Id, &seller.Cid, &seller.CompanyName, &seller.Address, &seller.Telephone, &seller.LocalityId)
+
+	if err != nil {
+		log.Println(err)
+		return database.Seller{}, err
 	}
-	return database.Seller{}, fmt.Errorf("error: seller with id %d not found", id)
+
+	return seller, nil
 }
 
-func (r *repository) Create(cid uint64, companyName string, address string, telephone string) (database.Seller, error) {
-	id := r.generateId()
-	data := createSeller(id, cid, companyName, address, telephone)
-	r.db = append(r.db, data)
-	return data, nil
+func (r *repository) Create(cid uint64, companyName string, address string, telephone string, localityId string) (database.Seller, error) {
+	db := database.StorageDB
+	stmt, err := db.Prepare(CreateQuery)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer stmt.Close()
+
+	var result sql.Result
+
+	result, err = stmt.Exec(cid, companyName, address, telephone, localityId)
+
+	if err != nil {
+		return database.Seller{}, err
+	}
+
+	insertedId, _ := result.LastInsertId()
+
+	insertedSeller := createSeller(uint64(insertedId), cid, companyName, address, telephone, localityId)
+
+	return insertedSeller, nil
 }
 
 func (r *repository) Update(seller database.Seller) (database.Seller, error) {
-	for idx, s := range r.db {
-		if s.Id == seller.Id {
-			r.db[idx] = seller
-			return r.db[idx], nil
-		}
+	db := database.StorageDB
+	stmt, err := db.Prepare(UpdateQuery)
+
+	if err != nil {
+		log.Println(err)
 	}
-	return database.Seller{}, fmt.Errorf("error: seller with id %d not found", seller.Id)
+
+	defer stmt.Close()
+	_, err = stmt.Exec(
+		seller.Cid,
+		seller.CompanyName,
+		seller.Address,
+		seller.Telephone,
+		seller.LocalityId,
+		seller.Id,
+	)
+
+	if err != nil {
+		return database.Seller{}, err
+	}
+
+	return seller, nil
 }
 
 func (r *repository) Delete(id uint64) error {
-	for idx, seller := range r.db {
-		if seller.Id == id {
-			r.db = append(r.db[:idx], r.db[idx+1:]...)
-			return nil
-		}
+	db := database.StorageDB
+
+	_, err := db.Exec(DeleteQuery, id)
+
+	if err != nil {
+		log.Println(err)
+		return err
 	}
-	return fmt.Errorf("error: seller with id %d not found", id)
+
+	return nil
 }
 
 func (r *repository) FindCid(cid uint64) bool {
-	for _, seller := range r.db {
-		if seller.Cid == cid {
-			return true
-		}
-	}
-	return false
+
+	var seller database.Seller
+
+	db := database.StorageDB
+
+	err := db.QueryRow(FindCidQuery, cid).Scan(&seller.Cid)
+
+	return err == nil
 }
 
-func createSeller(id uint64, cid uint64, companyName string, address string, telephone string) database.Seller {
+func createSeller(id uint64, cid uint64, companyName string, address string, telephone string, localityId string) database.Seller {
 	return database.Seller{
 		Id:          id,
 		Cid:         cid,
 		CompanyName: companyName,
 		Address:     address,
 		Telephone:   telephone,
+		LocalityId:  localityId,
 	}
-}
-
-func (r repository) generateId() uint64 {
-	initialId++
-	return initialId
 }
 
 func NewRepository(db []database.Seller) Repository {
