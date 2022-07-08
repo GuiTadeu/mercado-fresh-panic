b9de07602a853859987bafae0dc3eef49bf203f3
Willy De Jesus Passos <willy.passos@mercadolivre.com>
2022-07-08 20:53:12 -0300

    implemented  methodt update and existCardNumberIdl

--- internal/buyers/repository.go
+++ internal/buyers/repository.go
@@ -2,35 +2,35 @@ package buyers
 
 import (
 	"database/sql"
-	db "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
+	models "github.com/GuiTadeu/mercado-fresh-panic/cmd/server/database"
 	"log"
 )
 
 type BuyerRepository interface {
-	Create(cardNumberId, firstName, lastName string) (db.Buyer, error)
-	Get(id uint64) (db.Buyer, error)
-	GetAll() ([]db.Buyer, error)
+	Create(cardNumberId, firstName, lastName string) (models.Buyer, error)
+	Get(id uint64) (models.Buyer, error)
+	GetAll() ([]models.Buyer, error)
 	Delete(id uint64) error
 	getNextId() uint64
-	Update(id uint64, cardNumberId string, firstName string, lastName string) (db.Buyer, error)
-	ExistsBuyerCardNumberId(cardNumberId string) bool
+	Update(updatedBuyer models.Buyer) (models.Buyer, error)
+	ExistsBuyerCardNumberId(cardNumberId string) (bool, error)
 }
 
-func NewBuyerRepository(buyers []db.Buyer) BuyerRepository {
+type buyerRepository struct {
+	db *sql.DB
+}
+
+func NewBuyerRepository(db *sql.DB) BuyerRepository {
 	return &buyerRepository{
-		buyers: buyers,
+		db: db,
 	}
 
 }
 
-type buyerRepository struct {
-	buyers []db.Buyer
-}
+func (r *buyerRepository) Create(cardNumberId, firstName, lastName string) (models.Buyer, error) {
+	database := models.StorageDB
 
-func (r *buyerRepository) Create(cardNumberId, firstName, lastName string) (db.Buyer, error) {
-	database := db.StorageDB
-
-	stmt, err := database.Prepare("INSERT INTO buyer(buyer_code,cardNumberId, firstName, lastName) VALUES(?, ?, ?, ?)")
+	stmt, err := database.Prepare("INSERT INTO buyer(buyer_code,card_number_d, first_name, last_name) VALUES(?, ?, ?, ?)")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -39,11 +39,11 @@ func (r *buyerRepository) Create(cardNumberId, firstName, lastName string) (db.B
 	var result sql.Result
 	result, err = stmt.Exec(cardNumberId, firstName, lastName)
 	if err != nil {
-		return db.Buyer{}, err
+		return models.Buyer{}, err
 	}
 
 	insertedId, _ := result.LastInsertId()
-	buyer := db.Buyer{
+	buyer := models.Buyer{
 		Id:           uint64(insertedId),
 		CardNumberId: cardNumberId,
 		FirstName:    firstName,
@@ -65,31 +65,32 @@ func (r *buyerRepository) getNextId() uint64 {
 	return buyers[len(buyers)-1].Id + 1
 }
 
-func (r *buyerRepository) Get(id uint64) (db.Buyer, error) {
-	var buyer db.Buyer
-	database := db.StorageDB
-	err := database.QueryRow("SELECT id,buyer_code,cardNumberId, firstName, lastName FROM buyers WHERE id = ?",
+func (r *buyerRepository) Get(id uint64) (models.Buyer, error) {
+	var buyer models.Buyer
+	database := models.StorageDB
+	err := database.QueryRow("SELECT id,buyer_code,card_number_d, first_name, last_name FROM buyers WHERE id = ?",
 		id).Scan(&buyer.Id, &buyer.CardNumberId, &buyer.FirstName, &buyer.LastName)
 	if err != nil {
 		log.Println(err)
-		return db.Buyer{}, err
+		return models.Buyer{}, err
 	}
 	return buyer, nil
 }
 
-func (r *buyerRepository) GetAll() ([]db.Buyer, error) {
+func (r *buyerRepository) GetAll() ([]models.Buyer, error) {
 
-	database := db.StorageDB
-	stmt, err := database.Query("SELECT id,buyer_code,cardNumberId, firstName, lastName FROM buyers")
+	database := models.StorageDB
+	stmt, err := database.Query("SELECT * FROM buyers")
 	if err != nil {
+		log.Println(err)
 		return nil, err
 	}
 	defer stmt.Close()
 
-	var buyers []db.Buyer
+	var buyers []models.Buyer
 
 	for stmt.Next() {
-		var buyer db.Buyer
+		var buyer models.Buyer
 
 		if err = stmt.Scan(
 			&buyer.Id,
@@ -97,7 +98,8 @@ func (r *buyerRepository) GetAll() ([]db.Buyer, error) {
 			&buyer.FirstName,
 			&buyer.LastName,
 		); err != nil {
-			return r.buyers, err
+			log.Println(err)
+			return nil, err
 		}
 		buyers = append(buyers, buyer)
 	}
@@ -105,7 +107,7 @@ func (r *buyerRepository) GetAll() ([]db.Buyer, error) {
 }
 
 func (r *buyerRepository) Delete(id uint64) error {
-	database := db.StorageDB
+	database := models.StorageDB
 	stmt, err := database.Prepare("DELETE FROM buyers WHERE id = ?")
 	if err != nil {
 		log.Fatal(err)
@@ -119,22 +121,56 @@ func (r *buyerRepository) Delete(id uint64) error {
 	return nil
 }
 
-func (r *buyerRepository) Update(id uint64, cardNumberId string, firstName string, lastName string) (db.Buyer, error) {
-	uBuyer := db.Buyer{id, cardNumberId, firstName, lastName}
-	for index, buyer := range r.buyers {
-		if buyer.Id == id {
-			r.buyers[index] = uBuyer
-			return uBuyer, nil
-		}
+func (r *buyerRepository) Update(updatedBuyer models.Buyer) (models.Buyer, error) {
+
+	stmt, err := r.db.Prepare(`
+	UPDATE buyers SET 
+	buyers_code =?,
+	card_number_id =?,
+	first_name =?,
+	last_name =?,
+	where id =?
+`)
+	if err != nil {
+		return models.Buyer{}, err
 	}
-	return db.Buyer{}, BuyerNotFoundError
+
+	defer stmt.Close()
+	_, err = stmt.Exec(
+		updatedBuyer.Id,
+		updatedBuyer.CardNumberId,
+		updatedBuyer.FirstName,
+		updatedBuyer.LastName,
+	)
+
+	if err != nil {
+		return models.Buyer{}, err
+	}
+	return updatedBuyer, nil
 }
 
-func (r *buyerRepository) ExistsBuyerCardNumberId(cardNumberId string) bool {
-	for _, buyer := range r.buyers {
-		if buyer.CardNumberId == cardNumberId {
-			return true
-		}
+func (r *buyerRepository) ExistsBuyerCardNumberId(cardNumberId string) (bool, error) {
+	var buyer models.Buyer
+
+	database := models.StorageDB
+
+	stmt, err := database.Query("SELECT * FROM buyers WHERE card_number_id = ?", cardNumberId)
+
+	if err != nil {
+		return false, err
 	}
-	return false
+
+	for stmt.Next() {
+
+		err = stmt.Scan(
+			&buyer.Id,
+			&buyer.CardNumberId,
+			&buyer.FirstName,
+			&buyer.LastName,
+		)
+	}
+	if err != nil {
+		return false, err
+	}
+	return true, err
 }
