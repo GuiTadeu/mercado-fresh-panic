d833a41e44f47f60b088257b9de0d2b30859101b
Gian Raphael Martins Da Costa <gian.costa@mercadolivre.com>
2022-07-11 17:37:22 -0300

    merge pt1 sprint 3

--- internal/inboundOrders/service.go
+++ internal/inboundOrders/service.go
@@ -34,13 +34,13 @@ func NewInboundOrderService(employeeRepository employees.EmployeeRepository, war
 
 func (s *inboundOrderService) Create(orderDate, orderNumber string, employeeId, productBatchId, warehouseId uint64) (db.InboundOrder, error) {
 	// TODO adicionar validaçao se productBatchId existe
-	// if _, err := s.employeeService.Get(employeeId); err != nil {
-	// 	return db.InboundOrder{}, EmployeeNotFoundError
-	// }
+	 if !s.employeeRepository.ExistsEmployee(employeeId) {
+	 	return db.InboundOrder{}, EmployeeNotFoundError
+	 }
 
-	// if _, err := s.warehouseService.Get(warehouseId); err != nil {
-	// 	return db.InboundOrder{}, WarehouseNotFoundError
-	// }
+	 if _, err := s.warehouseRepository.Get(warehouseId); err != nil {
+	 	return db.InboundOrder{}, WarehouseNotFoundError
+	 }
 
 	inboundOrder, err := s.inboundOrderRepository.Create(orderDate, orderNumber, employeeId, productBatchId, warehouseId)
 	if err != nil {
